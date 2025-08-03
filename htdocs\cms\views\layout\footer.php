4ff81a570043755a1066546af93a9b40db1ddd11
Javadictator <33174726+Javadictator@users.noreply.github.com>
2025-08-03 21:22:57 +0700

    Create htdocs\cms\views\layout\footer.php

---
+++ htdocs\cms\views\layout\footer.php
@@ -0,0 +1,10 @@
+
+<?php
+// views/layout/footer.php
+?>
+</div> <!-- .d-flex -->
+<script
+  src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js">
+</script>
+</body>
+</html>
